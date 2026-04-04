7ffd4c5b7e00324f539a2f8ee14e2cbbfc7a594f
sakshi <sakshi0981456@gmail.com>
2026-04-04 09:25:23 +0530

    Updated project files

---
+++ sync.js
@@ -0,0 +1,11 @@
+console.log("Synchronous Code")
+
+var a = 62656954;
+var b = 6256546;
+
+function multiply(a, b) {
+    const result = a * b
+    return result
+}
+var c = multiply(a, b);
+console.log("Multiplication of a and b is " + c)
